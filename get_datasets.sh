a4fdcf62a3d8fe7d311ea8c41c21a6eda6640e20
NickShu <nicolas.s.shu@gmail.com>
2019-04-04 22:02:11 -0400

    Fix get_datasets

--- get_datasets.sh
+++ get_datasets.sh
@@ -1,5 +1,5 @@
 mkdir datasets
-cd datasets 
+cd datasets
 wget http://festvox.org/cmu_arctic/cmu_arctic/packed/cmu_us_awb_arctic-0.95-release.zip
 wget http://festvox.org/cmu_arctic/cmu_arctic/packed/cmu_us_bdl_arctic-0.95-release.zip
 wget http://festvox.org/cmu_arctic/cmu_arctic/packed/cmu_us_clb_arctic-0.95-release.zip
@@ -9,19 +9,18 @@ wget http://festvox.org/cmu_arctic/cmu_arctic/packed/cmu_us_rms_arctic-0.95-rele
 wget http://festvox.org/cmu_arctic/cmu_arctic/packed/cmu_us_slt_arctic-0.95-release.zip
 
 
-mkdir awb
-mkdir bdl
-mkdir clb 
-mkdir jmk
-mkdir ksp
-mkdir rms
-mkdir slt
+unzip cmu_us_awb_arctic-0.95-release.zip
+unzip cmu_us_bdl_arctic-0.95-release.zip
+unzip cmu_us_clb_arctic-0.95-release.zip
+unzip cmu_us_jmk_arctic-0.95-release.zip
+unzip cmu_us_ksp_arctic-0.95-release.zip
+unzip cmu_us_rms_arctic-0.95-release.zip
+unzip cmu_us_slt_arctic-0.95-release.zip
 
-unzip cmu_us_awb_arctic-0.95-release.zip -d awb
-unzip cmu_us_bdl_arctic-0.95-release.zip -d bld
-unzip cmu_us_clb_arctic-0.95-release.zip -d clb
-unzip cmu_us_jmk_arctic-0.95-release.zip -d jmk
-unzip cmu_us_ksp_arctic-0.95-release.zip -d ksp
-unzip cmu_us_rms_arctic-0.95-release.zip -d rms
-unzip cmu_us_slt_arctic-0.95-release.zip -d slt
-
+mv cmu_us_awb_arctic awb
+mv cmu_us_bdl_arctic bdl
+mv cmu_us_clb_arctic clb
+mv cmu_us_jmk_arctic jmk
+mv cmu_us_ksp_arctic ksp
+mv cmu_us_rms_arctic rms
+mv cmu_us_slt_arctic slt
