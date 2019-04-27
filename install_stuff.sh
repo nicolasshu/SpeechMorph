24c76926fe529b18f0029b266a600897c2b14af3
Nick <nicolas.s.shu@gmail.com>
2019-04-27 13:04:05 -0400

    Update install_stuff.sh

--- install_stuff.sh
+++ install_stuff.sh
@@ -2,4 +2,4 @@
 sudo apt update
 sudo apt install python3 python3-pip
 
-pip install numpy IPython scipy matplotlib python_speech_features fastdtw tqdm seaborn torch sklearn pandas datetime
+pip3 install numpy IPython scipy matplotlib python_speech_features fastdtw tqdm seaborn torch sklearn pandas datetime
